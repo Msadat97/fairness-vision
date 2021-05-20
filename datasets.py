91b1012e3b102fbb75057d7cc68356429010b566
Seyedmorteza Sadat <71992433+Msadat97@users.noreply.github.com>
2021-05-20 22:14:10 +0200

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,49 +1,29 @@
-from os.path import split
-from torch.cuda.random import seed
-from torch.utils import data
-from torch.utils.data import Dataset, DataLoader
-import torch
-from torchvision.datasets import MNIST, CelebA
-from models import VAE
-import os
 from pathlib import Path
-import os
+from models import BaseVAE
+from torch.utils.data import Dataset
+from torchvision.datasets import MNIST, CelebA
+import torch
 
 data_path = Path.cwd().joinpath('data')
 data_path.mkdir(parents=True, exist_ok=True)
 
 
 class CustomMNIST(Dataset):
-    def __init__(self, train, vae: VAE = None, with_channel=False) -> None:
+    def __init__(self, train, with_channel=False) -> None:
         super().__init__()
         self.dataset = MNIST(root=str(data_path), train=train, download=True)
-        self.vae = vae
         self.with_channel = with_channel
         
     def __len__(self):
-        return len(self.dataset.data)
+        return len(self.dataset)
     
     def __getitem__(self, index):
         data = self.dataset.data[index]
-        data = data/255.0
         targets = self.dataset.targets[index]
-        
-        if self.vae is not None:
-            data = data[None, None, ...] if isinstance(index, int) else data[:, None, ...]
-            data = self._get_latents(data)[0]
-            return data, targets
-        else:
-            if self.with_channel:
-                data = data[None, ...] if isinstance(index, int) else data[:, None, ...]
-            return data, targets
-
-    @torch.no_grad()
-    def _get_latents(self, data):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        data = data.to(device)
-        vae = self.vae.to(device)
-        z_batch, _, _ = self.vae.encode(data)
-        return z_batch.cpu()
+        data = data/255.0
+        if self.with_channel:
+            data = data[None, ...] if isinstance(index, int) else data[:, None, ...]
+        return data, targets
 
 
 class CustomCelebA(Dataset):
@@ -52,5 +32,28 @@ class CustomCelebA(Dataset):
         self.data = CelebA(root=str(data_path), split=split, download=True)
 
 
-t = CustomCelebA()
+class VAEWrapper(Dataset):
+    def __init__(self, vae: BaseVAE, dataset: Dataset):
+        super(VAEWrapper, self).__init__()
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.dataset = dataset
+        self.vae = vae.to(self.device)
 
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        data, targets = self.dataset[index]
+        if isinstance(index, int):
+            data = data[None, ...]
+        if data.ndim == 3:
+            data = data[None, ...]
+        assert data.ndim == 4
+        latents = self._get_latents(data)
+        return latents, targets
+
+    @torch.no_grad()
+    def _get_latents(self, data):
+        data = data.to(self.device)
+        z_batch, _, _ = self.vae.encode(data)
+        return z_batch.cpu()
